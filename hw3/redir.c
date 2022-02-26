fd261d3e5c26fa5773030f4759b3df3239ac02bb
Daniel <ngo8@stolaf.edu>
2022-02-26 16:22:49 -0600

    WS7B complete

---
+++ hw3/redir.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>  
+#include <sys/types.h>
+#include <cstdlib>
+
+int main(int argc, char **argv, char **envp) {
+  char *array = (char*) malloc(20); 
+  printf("Enter File Name: ");
+  scanf("%19s", array);
+  //printf("%s\n", array);
+  int ret, ret1, dup1, closereturn;
+
+  if ((ret = open(array, O_RDONLY)) < 0) {
+        perror("open() failed");
+        return -1;
+    }
+  
+  if ((closereturn = close(0)) < 0) {
+        perror("close");
+        return -1;
+    }
+
+  dup1 = dup(ret);
+
+  if ((ret1 = execve("/bin/grep", argv, envp)) < 0) {
+    perror("execve");
+    return -1;
+  }
+
+  return 0;
+}
