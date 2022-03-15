671da011238902185b6225581e83bd6f21b61cc9
Daniel <ngo8@stolaf.edu>
2022-03-15 00:32:38 -0500

    shell project

--- pp-shell/extra.c
+++ pp-shell/extra.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <ctype.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>  
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 char *get_token(char **ptr, int *status) {
     char *tok = (char*) malloc(80); //dynamic array
@@ -27,8 +31,8 @@ char *get_token(char **ptr, int *status) {
 }
 
 int main(int argc, char **argv, char **envp) {
-    int loop = 1;
-    while (loop == 1) {
+    int loop = 1; 
+    while (loop == 1) { //loop for program
     char *line = (char*) malloc(80);
     char *baseline = line;
     size_t size = 80;
@@ -58,6 +62,14 @@ int main(int argc, char **argv, char **envp) {
         }
     }
 
+    int fd[2];
+
+    if (pipe(fd) < 0 ) {
+        perror( "pipe" );
+        return -1;
+    }
+
+
     int fork1;
 
     if ((fork1 = fork()) < 0) {
@@ -69,13 +81,76 @@ int main(int argc, char **argv, char **envp) {
     if (fork1 != 0) {
         wait(NULL);
         printf("\nChild Process Exited Safely\n");
-    } else {
-        int ret2;
-        printf("Child Process Was A Success With An ID Of %d And Parent ID Of %d\n", getpid(), getppid());
-        if ((ret2 = execve(arr[0], arr, envp)) < 0) {
-            perror("execve");
+
+        if((close(fd[1])) < 0) {
+            perror("close");
             return -1;
         }
+
+        dup(fd[0]);
+        char buffer[50];
+        int n = read(fd[0], buffer, sizeof(buffer));
+
+        if (buffer[0] != '\0') {
+            if ((chdir(buffer)) < 0) {
+                perror("chdir");
+                return -1;
+            }
+        }
+
+    } else {
+        int ret2, fd2, dup1;
+
+        //printf("Child Process Was A Success With An ID Of %d And Parent ID Of %d\n", getpid(), getppid());
+
+        char output;
+        printf("Do you want the output to be directed to an output file? \nType y or n \nResponse: ");
+        scanf("%c", &output);
+
+        if (output == 'y') {
+            if ((fd2 = open("output.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+            perror("open");
+            return -1;
+            }
+
+            if ((close(1)) < 0) {
+                perror("close");
+                return -1;
+            }
+
+            dup1 = dup(fd2);
+
+            close(fd2); 
+
+        }
+        if (strcmp(arr[0], "exit") == 0) {
+            kill(getppid(), SIGKILL);
+            return -1;
+        }
+        
+        if (strcmp(arr[0], "/bin/cd") == 0) {
+
+            if ((close(fd[0])) < 0) {
+                perror("close");
+                return -1;
+            }
+
+            if ((close(1)) < 0) {
+                perror("close");
+                return -1;
+            }
+
+            dup(fd[1]);
+            write(1, arr[1], 50);
+
+        } else {
+            if ((ret2 = execve(arr[0], arr, envp)) < 0) {
+                perror("execve");
+                return -1;
+            }
+        }
+
+        _exit(1);
     }
 
     arr[count] = (char*)0;
