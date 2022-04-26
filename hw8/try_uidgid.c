20dd4d33d06f328c701c3b102f937ccfea0073c6
Daniel <ngo8@stolaf.edu>
2022-04-26 14:37:47 -0500

    WS24C almost complete

---
+++ hw8/try_uidgid.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int realuserid, realgroupid, effectiveuserid, effectivegroupid;
+    if((realuserid = getuid()) < 0) {
+        perror("getuid()");
+        return -1;
+    }
+    printf("real user ID: %d\n", realuserid);
+
+    if((realgroupid = getgid()) < 0) {
+        perror("getgid()");
+        return -1;
+    }
+    printf("real group ID: %d\n", realgroupid);
+
+    if((effectiveuserid = geteuid()) < 0) {
+        perror("geteuid()");
+        return -1;
+    }
+    printf("effective real user ID: %d\n", effectiveuserid);
+
+    if((effectivegroupid = getuid()) < 0) {
+        perror("getegid()");
+        return -1;
+    }
+    printf("effective real group ID: %d\n", effectivegroupid);
+
+    uid_t uid = 1;
+    gid_t gid = 2;
+    uid_t euid = 3;
+    gid_t egid = 4;
+    int ret1, ret2, ret3, ret4;
+
+    if((ret1 = setuid(uid)) < 0) {
+        perror("setuid()");
+        return -1;
+    }
+    //printf("real user ID: %d\n", realuserid = getuid());
+
+    if((ret2 = setgid(gid)) < 0) {
+        perror("setgid()");
+        return -1;
+    }
+    //printf("real group ID: %d\n", realgroupid = getgid());
+
+    if((ret3 = seteuid(euid)) < 0) {
+        perror("seteuid()");
+        return -1;
+    }
+    //printf("effective real user ID: %d\n", effectiveuserid = geteuid());
+
+    if((ret4 = setuid(egid)) < 0) {
+        perror("setegid()");
+        return -1;
+    }
+    //printf("effective real group ID: %d\n", effectivegroupid = getuid());
+
+
+
+    // int processid;
+    // if ((processid = getpid()) < 0) {
+    //     perror("getpid");
+    //     return -1;
+    // }
+    // printf("getpid() returned %d \n", processid);
+
+}
